5d35614c05142882a691dea4f17417b931b26f40
ahmedhammam <ahmed@hammam.me>
2024-02-19 01:53:10 +0300

    rewrite the class

--- src/book/ds/interview/ch2/dynamic/programig/EditDistance.java
+++ src/book/ds/interview/ch2/dynamic/programig/EditDistance.java
@@ -1,43 +1,42 @@
 package book.ds.interview.ch2.dynamic.programig;
 
 public class EditDistance {
-	public static int editDistance(String str1, String str2) {
-		int m = str1.length();
-		int n = str2.length();
-		// Create a matrix to store the edit distances
-		int[][] matrix = new int[m + 1][n + 1];
+    // Function to calculate the minimum edit distance between two strings
+    public static int editDistance(String str1, String str2, int m, int n) {
+        // Create a matrix to store the edit distances between substrings of str1 and str2
+        int[][] matrix = new int[m + 1][n + 1];
 
-		// Fill in the matrix
-		for (int i = 0; i <= m; i++) {
-			for (int j = 0; j <= n; j++) {
-				// If first string is empty, insert all characters of second string
-				if (i == 0) {
-					matrix[i][j] = j;
-				}
-				// If second string is empty, remove all characters of first string
-				else if (j == 0) {
-					matrix[i][j] = i;
-				}
-				// If last characters are same, ignore last character and recur for remaining
-				// string
-				else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-					matrix[i][j] = matrix[i - 1][j - 1];
-				}
-				// If last character are different, consider all possibilities and find minimum
-				else {
-					matrix[i][j] = 1 + Math.min(matrix[i][j - 1], // Insert
-							Math.min(matrix[i - 1][j], // Remove
-									matrix[i - 1][j - 1])); // Replace
-				}
-			}
-		}
+        // Fill the matrix using dynamic programming
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                // If one of the strings is empty, the edit distance is the length of the other string
+                if (i == 0) {
+                    matrix[i][j] = j;
+                } else if (j == 0) {
+                    matrix[i][j] = i;
+                }
+                // If the characters at the current positions are the same, no operation needed
+                else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    matrix[i][j] = matrix[i - 1][j - 1];
+                }
+                // Otherwise, find the minimum of insert, remove, or replace operations
+                else {
+                    matrix[i][j] = 1 + Math.min(Math.min(matrix[i][j - 1], // Insert
+                                    matrix[i - 1][j]), // Remove
+                            matrix[i - 1][j - 1]); // Replace
+                }
+            }
+        }
+        // Return the edit distance between the two strings
+        return matrix[m][n];
+    }
 
-		return matrix[m][n];
-	}
+    public static void main(String[] args) {
+        // Input strings
+        String str1 = "sunday";
+        String str2 = "saturday";
 
-	public static void main(String[] args) {
-		String str1 = "sunday";
-		String str2 = "saturday";
-		System.out.println(editDistance(str1, str2));
-	}
+        // Calculate and print the minimum edit distance between the strings
+        System.out.println("Minimum Edit Distance: " + editDistance(str1, str2, str1.length(), str2.length()));
+    }
 }
